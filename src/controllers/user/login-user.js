1874ac9946be26dbe4b520c266a11a1bacb68e14
Allaaxx <allanrao358@gmail.com>
2026-07-14 09:08:53 -0300

    feat: add Login User Cointroller

---
+++ src/controllers/user/login-user.js
@@ -0,0 +1,49 @@
+import { ZodError } from 'zod';
+import { loginSchema } from '../../schemas/index.js';
+import {
+    serverError,
+    badRequest,
+    ok,
+    unauthorized,
+    notFound,
+} from '../helpers/index.js';
+import { InvalidPasswordError, UserNotFoundError } from '../../errors/user.js';
+
+export class LoginUserController {
+    constructor(loginUserUseCase) {
+        this.loginUserUseCase = loginUserUseCase;
+    }
+
+    async execute(httpRequest) {
+        try {
+            const params = httpRequest.body;
+            await loginSchema.parseAsync(params);
+            const user = await this.loginUserUseCase.execute(
+                params.email,
+                params.password,
+            );
+
+            return ok(user);
+        } catch (error) {
+            if (error instanceof ZodError) {
+                return badRequest({
+                    message: error.issues[0].message,
+                });
+            }
+
+            if (error instanceof InvalidPasswordError) {
+                return unauthorized({
+                    message: error.message,
+                });
+            }
+
+            if (error instanceof UserNotFoundError) {
+                return notFound({
+                    message: 'User not found',
+                });
+            }
+
+            return serverError();
+        }
+    }
+}
--- src/schemas/user.js
+++ src/schemas/user.js
@@ -38,3 +38,22 @@ export const createUserSchema = z.object({
 export const updatedUserSchema = createUserSchema.partial().strict({
     message: 'Some provided field is not allowed.',
 });
+
+export const loginSchema = z.object({
+    email: z
+        .email({
+            message: 'Please provide a valid e-mail.',
+        })
+        .trim()
+        .min(1, {
+            message: 'E-mail is required.',
+        }),
+    password: z
+        .string({
+            required_error: 'Password is required',
+        })
+        .trim()
+        .min(6, {
+            message: 'Password must have at least 6 characters',
+        }),
+});
